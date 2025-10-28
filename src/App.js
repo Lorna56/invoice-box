0a2ac2868ede6a09f427c3101d3e01728b90ec64
Lorna56 <lornanaula0042@gmail.com>
2025-10-28 20:40:07 +0300

    Set up private component file, and suth context file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ProviderDashboard from './pages/ProviderDashboard';
+import PurchaserDashboard from './pages/PurchaserDashboard';
+import InvoiceDetails from './pages/InvoiceDetails';
+import CreateInvoice from './pages/CreateInvoice';
+import Payment from './pages/Payment';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/" element={<PrivateRoute><ProviderDashboard /></PrivateRoute>} />
+              <Route path="/provider-dashboard" element={<PrivateRoute><ProviderDashboard /></PrivateRoute>} />
+              <Route path="/purchaser-dashboard" element={<PrivateRoute><PurchaserDashboard /></PrivateRoute>} />
+              <Route path="/invoice/:id" element={<PrivateRoute><InvoiceDetails /></PrivateRoute>} />
+              <Route path="/create-invoice" element={<PrivateRoute><CreateInvoice /></PrivateRoute>} />
+              <Route path="/payment/:invoiceId" element={<PrivateRoute><Payment /></PrivateRoute>} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
