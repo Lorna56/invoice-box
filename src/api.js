c98c1f93fec92501705ee6dadf297a73192000b3
Lorna56 <lornanaula0042@gmail.com>
2025-11-01 09:14:11 +0300

    Still trying to deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://invoice-box-api.onrender.com/';
 
 console.log('API Base URL:', API_BASE_URL); // Debug log
 
